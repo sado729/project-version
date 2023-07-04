f84010515f0f6375a721b37cdbdb8f80eb52ac1e
Sadiq <sadiqmemmedov93@mail.ru>
2023-07-05 01:39:14 +0400

    table name add model

--- src/Models/Information.php
+++ src/Models/Information.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Information extends Model
 {
     public $timestamps = false;
+    public $table = 'informations';
     protected $fillable = ['version'];
 }
